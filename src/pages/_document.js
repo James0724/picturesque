cf29c3eabd0e148d0edbf4d5abb3317c4f3fe16e
james0724 <jameskahoro07@gmail.com>
2023-04-13 15:45:41 +0300

    second optimization trials

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=public-sans@300&f[]=bonny@700,500,400&f[]=gambarino@400&f[]=melodrama@300,400,600,700,500&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
